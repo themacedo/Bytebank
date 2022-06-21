77ae4e156e091847dadcc35acb8e957a13b3e374
mmacedo721@gmal.com <tereza0816>
2022-06-21 17:48:10 -0300

    Kotlin: lidando com exceptions e referências nulas

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,34 +1,55 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Cliente
 import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.teste.testaFuncionarios
+import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun main() {
-    val endereco = Endereco(
-        logradouro = "Rua vergueiro",
-        complemento = "Alura",
-        cep = "00000-0700"
-    )
-    val enderecoNovo = Endereco(
-        logradouro = "Rua vergueiro",
-        complemento = "Alura",
-        cep = "00000-0700"
-    )
+    println("início main")
+    val entrada: String = "1.0"
 
-    println(endereco.equals(enderecoNovo))
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
 
-    println(endereco.hashCode())
-    println(enderecoNovo.hashCode())
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
 
-    println(endereco)
-    println(enderecoNovo)
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
 
-    println("${endereco.javaClass}@${
-    Integer.toHexString(endereco.hashCode())}")
+
+    funcao1()
+    println("fim main")
 }
 
-fun imprime(valor: Any): Any {
-    println(valor)
-    return valor
+fun funcao1() {
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        e.printStackTrace()
+        println("ClassCastException foi pegada")
+    }
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
 }
